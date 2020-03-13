45f8c94a5768eacc6adc728c45387033a7f3fed1
Rustam Sharshenov <rustam@sharshenov.com>
2020-03-13 14:17:38 +0000

    Filter Job ID and timestamp from test logs
    Rails 5+ adds unique Job ID and timestamp to logs what makes tests matchers failing

--- spec/support/log_helpers.rb
+++ spec/support/log_helpers.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
 module LogHelpers
-  def logs(name)
-    File.open(logs_path.join("#{name}.log")).read
+  def logs(name, filters: [:job_id, :created_at])
+    text = File.open(logs_path.join("#{name}.log")).read
+
+    {
+      job_id: /\(Job ID[^\)]+\) /,
+      created_at: /enqueued at [^ ]+ /
+    }.each do |filter, regex|
+      text.gsub!(regex, '') if filters.include?(filter)
+    end
+
+    text
   end
 
   def cleanup_logs

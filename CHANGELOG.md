98024b2110e15c260540bf5c5f3601256f5c65c0
Rustam Sharshenov <rustam@sharshenov.com>
2020-04-04 18:00:34 +0100

    Actualize worker class name in rake task description

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## Changes Between 0.2.0 and 0.2.1 (unreleased)
+
+Fixed worker class name in rake task description
+
 ## Changes Between 0.1.0 and 0.2.0
 
 ### `message_options` https://github.com/veeqo/advanced-sneakers-activejob/pull/1
--- lib/advanced_sneakers_activejob/tasks.rb
+++ lib/advanced_sneakers_activejob/tasks.rb
@@ -5,7 +5,7 @@ require 'sneakers/tasks'
 task :environment
 
 namespace :sneakers do
-  desc 'Start work for ActiveJob only (set $WORKERS=ActiveJobKlass1::Consumer,ActiveJobKlass2::Consumer)'
+  desc 'Start work for ActiveJob only (set $WORKERS=AdvancedSneakersActiveJob::FooQueueConsumer for processing of :foo queue)'
   task :active_job do
     Rake::Task['environment'].invoke
 

214715c2c2c9b715b8eabf412a70c75de09fdbc9
Rustam Sharshenov <rustam@sharshenov.com>
2020-04-05 12:36:39 +0100

    Remove redundant puts from specs

--- spec/integration/consumers_spec.rb
+++ spec/integration/consumers_spec.rb
@@ -37,7 +37,7 @@ describe 'Consumers' do
 
   context 'when ActiveJob has prefix defined' do
     subject do
-      r = in_app_process(adapter: :advanced_sneakers, env: { 'ACTIVE_JOB_QUEUE_NAME_PREFIX' => 'custom', 'ACTIVE_JOB_QUEUE_NAME_DELIMITER' => ':' }) do
+      in_app_process(adapter: :advanced_sneakers, env: { 'ACTIVE_JOB_QUEUE_NAME_PREFIX' => 'custom', 'ACTIVE_JOB_QUEUE_NAME_DELIMITER' => ':' }) do
         class FooJob < ApplicationJob
           queue_as :baz
         end
@@ -56,8 +56,6 @@ describe 'Consumers' do
 
         Sneakers::Worker::Classes.call.map(&:name)
       end
-      puts r.last
-      r
     end
 
     it 'are defined per queue with prefix ignored' do

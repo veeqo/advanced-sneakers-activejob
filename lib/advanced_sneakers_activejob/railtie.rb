0b9f27e4c556b3e7ea88aa1c06cbab3940c68d89
Rustam Sharshenov <rustam@sharshenov.com>
2020-07-27 17:25:26 +0100

    Drop Railtie initializer
    The Railtie initializer makes the patch to be applied much later than ActiveJob is loaded.
    It could lead to a NoMethodError if something unusual happens during Rails application load (like calling `Rails.application.eager_load!` in an initializer)
    
    ```
    NoMethodError: undefined method `message_options' for MyJob:Class
    ```

--- lib/advanced_sneakers_activejob/railtie.rb
+++ lib/advanced_sneakers_activejob/railtie.rb
@@ -3,18 +3,6 @@
 module AdvancedSneakersActiveJob
   # Rails integration
   class Railtie < ::Rails::Railtie
-    initializer 'advanced_sneakers_activejob.discover_mailer_job' do
-      ActiveSupport.on_load(:action_mailer) do
-        require 'action_mailer/delivery_job' # Enforce definition of ActionMailer::DeliveryJob::Consumer
-      end
-    end
-
-    initializer 'advanced_sneakers_activejob.discover_default_job' do
-      ActiveSupport.on_load(:active_job) do
-        ActiveJob::Base.include AdvancedSneakersActiveJob::ActiveJobPatch
-      end
-    end
-
     rake_tasks do
       require 'advanced_sneakers_activejob/tasks'
     end

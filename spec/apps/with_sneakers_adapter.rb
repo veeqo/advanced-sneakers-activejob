f3adb7159fd6a8dbc64f2f048ea81f504e8c3b06
Adis Osmonov <adis.osmonov@gmail.com>
2026-04-08 15:43:26 +0100

    Fix Rails 7.2 test app boot failure due to HealthController
    Rails 7.2 introduced Rails::HealthController which inherits from
    ActionController::Base. During eager_load!, it tries to load this
    controller, causing NameError when action_controller/railtie is not
    required. Add the require to all three test app files.

--- spec/apps/with_sneakers_adapter.rb
+++ spec/apps/with_sneakers_adapter.rb
@@ -3,6 +3,7 @@
 require 'bundler/setup'
 require 'rails'
 require 'active_job/railtie'
+require 'action_controller/railtie'
 require 'action_mailer/railtie' unless ENV['SKIP_MAILER']
 
 require 'sneakers'
